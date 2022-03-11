1375c810860421420cd748cce59c62126f486e92
Roy Sandoval <roysandoval@Roys-MacBook-Air.local>
2022-03-10 21:35:57 -0700

    Initial commit

---
+++ contracts/FirstNFT.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract FirstNFT {
+    constructor(){
+        console.log("This is my first NFT contract. Whoa!");
+    }
+}
